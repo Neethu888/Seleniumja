26ff8905326d8d9bd3c50861386e607be99d1cdb
Neethu888 <nidhinraju1612@gmail.com>
2024-07-27 01:13:01 +0530

    Add files via upload

---
+++ homeassignment1/Chrome.java
@@ -0,0 +1,19 @@
+package homeassignment1;
+
+public class Chrome {
+public static void main(String[] args) {
+      float versionType = 91.0f;
+      String developerType = "Google";
+      boolean isBetavalue = false;
+      int releaseyear = 2008;
+      char shortcutkey = 'C';
+      
+      System.out.println("The version type is" +versionType);
+      System.out.println("The developer type is" +developerType);
+      System.out.println("The Beta value is" +isBetavalue);
+      System.out.println("The release year is" +releaseyear);
+      System.out.println("The shortcutkey is" +shortcutkey);
+}
+
+
+}
